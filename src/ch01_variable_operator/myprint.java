4f11e081bfd8a4aa6edceced3bb9f1f958e468b8
jin <jin@naver.com>
2025-07-23 14:27:48 +0900

    real frist test

--- src/ch01_variable_operator/myprint.java
+++ src/ch01_variable_operator/myprint.java
@@ -1,4 +1,26 @@
 package ch01_variable_operator;
 
+/*
+    작성자:홍길동
+    작성일;:7월23일
+    목적: 처음 해보는 자바 프로그래밍
+*/
 public class myprint {
+    public static void main(String[] args) {
+        // 한줄 짜리 주석(comment)
+        System.out.println("홍"); // 성을 출력합니다.
+        System.out.println();     // 엔터 누릅니다.
+        System.out.println("길"); // 이름을 출력합니다.
+        System.out.println("동");
+
+        System.out.print("대한 민국");
+        System.out.print(" 화이팅");
+        System.out.println();
+        System.out.println("\"예체능\" 프로그램 메인 MC \'강호동\'");
+
+        System.out.println();
+        System.out.println("이름 \t국어\t영어\t수학");
+        System.out.println("김철수\t58\t68\t78");
+        System.out.println("박명희\t38\t48\t58");
+    }
 }
